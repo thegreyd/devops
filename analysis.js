3f95ec64048445cc0e9720498a01cc3c7454f712
kmurudi <kmurudi@ncsu.edu>
2017-10-18 01:44:57 -0400

    ADDEd two condition checks - to detect bigO>2 and long methods >120 added two checks - bigO>3 and long methods >120

--- analysis.js
+++ analysis.js
@@ -1,4 +1,7 @@
 var random = require("random-js")();
+var fs = require('fs');
+//var esprima = require('esprima');
+//var estraverse = require('estraverse');
 
 // Assign passed or failed randomly
 if (random.integer(1,100) < 50) {
