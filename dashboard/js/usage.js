4d3cb0b3c70032e4c97e89e5f4c887c909fb755f
Siddharth Sharma <sid.sharma0@gmail.com>
2017-12-03 17:55:12 -0500

    major updates

--- dashboard/js/usage.js
+++ dashboard/js/usage.js
@@ -1,28 +1,13 @@
 $(function() {
-
+    var content = $('#json').text();
     Morris.Area({
         element: 'usage-chart',
-        data: [{
-            period: '00:00',
-            cpu: 60
-        }, {
-            period: '01:00',
-            cpu: 70
-        }, {
-            period: '02:00',
-            cpu: 90
-        }, {
-            period: '03:00',
-            cpu: 40
-        }, {
-            period: '04:00',
-            cpu: 60
-        }],
-        xkey: 'period',
+        data: JSON.parse(content),
+        xkey: 'timestamp',
         // ykeys: ['iphone', 'ipad', 'itouch'],
-        ykeys: ['cpu'],
+        ykeys: ['cpu','memory','disk'],
         // labels: ['iPhone', 'iPad', 'iPod Touch'],
-        labels: ['CPU Usage'],
+        labels: ['CPU Usage','Memory Usage', 'Disk Usage'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
--- dashboard/main.js
+++ dashboard/main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var request = require('request')
-var templ = require('simple-html-template')
+var mustacheExpress = require('mustache-express');
 
 app.use('/data', express.static(path.join(__dirname, 'data')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
@@ -11,46 +11,45 @@ app.use('/less', express.static(path.join(__dirname, 'less')));
 app.use('/pages', express.static(path.join(__dirname, 'pages')));
 app.use('/vendor', express.static(path.join(__dirname, 'vendor')));
 
-app.engine('html', templ)
-app.set('views', 'pages')
-app.set('view engine', 'html')
-app.set('open_tag', '<?')
-app.set('close_tag', '/?>')
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/pages')
 
 app.get('/dependency', function(req, res){
 	request('http://54.201.183.218:3002/pingdependency', function (error, response, body) {
 		data = "";
 		JSON.parse(response["body"]).forEach(function(row) {
 			data+=	"<tr>" + 
-					"<td>" + row["moduleName"] + "</td>" + 
-					"<td>" + row["unused"] + "</td>" + 
-					"<td>" + row["latest"] + "</td>" + 
-					"<td>" + row["installed"] + "</td>" + 
-					"<td>" + row["bump"] + "</td>" + 
-					"</tr>"
+			"<td>" + row["moduleName"] + "</td>" + 
+			"<td>" + row["unused"] + "</td>" + 
+			"<td>" + row["latest"] + "</td>" + 
+			"<td>" + row["installed"] + "</td>" + 
+			"<td>" + row["bump"] + "</td>" + 
+			"</tr>"
 		});
 		res.render('dependency', {message: data});
-		//res.sendfile('pages/dependency.html');
 	})
 	
 });
 
 app.get('/usage', function(req, res){
+	var getData = function(db, callback) {
+		var collection = db.collection('ping');
+		collection.find({}, {timestamp:1, cpu:1, memory:1, disk:1}).sort({_id:1}).limit(50).toArray(function(err, result) {
+			callback(result);
+		});
+	}
 	var MongoClient = require('mongodb').MongoClient
- 	var assert = require('assert');
 	// Connection URL
 	var url = 'mongodb://localhost:27017/orkdb';
 	// Use connect method to connect to the Server
 	var data;
 	MongoClient.connect(url, function(err, db) {
-	  assert.equal(null, err);
-	  //data = db.ping.count()
-	  console.log("Connected correctly to server");
-	 
-	  db.close();
-	
-	res.render('usage', {message: data});
-});
+		getData(db, function(data) {
+			res.render('usage', {message: JSON.stringify(data)})
+			db.close();
+		});
+	});
 })
 
 app.get('/', function(req, res) {
