7b624ff8413258281df1cea04965b9f8a2a9692f
Siddharth Sharma <sid.sharma0@gmail.com>
2017-12-03 21:08:12 -0500

    minor updates

--- dashboard/js/flame.js
+++ dashboard/js/flame.js
@@ -2,5 +2,4 @@ $('#from').change(function () {
 	var val = $('#from').val();
 	$("#perf").attr('src',"data/perf-kernel_"+val+".svg")
 	$("#idle").attr('src',"data/nonidle_"+val+".svg")
-	alert(val)
 })
--- dashboard/js/usage.js
+++ dashboard/js/usage.js
@@ -1,13 +1,37 @@
 $(function() {
     var content = $('#json').text();
     Morris.Area({
-        element: 'usage-chart',
+        element: 'disk-chart',
         data: JSON.parse(content),
         xkey: 'timestamp',
         // ykeys: ['iphone', 'ipad', 'itouch'],
-        ykeys: ['cpu','memory','disk'],
+        ykeys: ['disk'],
         // labels: ['iPhone', 'iPad', 'iPod Touch'],
-        labels: ['CPU Usage','Memory Usage', 'Disk Usage'],
+        labels: ['Disk Usage'],
+        pointSize: 2,
+        hideHover: 'auto',
+        resize: true
+    });
+    Morris.Area({
+        element: 'memory-chart',
+        data: JSON.parse(content),
+        xkey: 'timestamp',
+        // ykeys: ['iphone', 'ipad', 'itouch'],
+        ykeys: ['memory'],
+        // labels: ['iPhone', 'iPad', 'iPod Touch'],
+        labels: ['Memory Usage'],
+        pointSize: 2,
+        hideHover: 'auto',
+        resize: true
+    });
+    Morris.Area({
+        element: 'cpu-chart',
+        data: JSON.parse(content),
+        xkey: 'timestamp',
+        // ykeys: ['iphone', 'ipad', 'itouch'],
+        ykeys: ['cpu'],
+        // labels: ['iPhone', 'iPad', 'iPod Touch'],
+        labels: ['CPU Usage'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
