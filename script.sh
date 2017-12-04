6c9207749d40ab6fc1ab98ca0f58382ca3adf914
ssharm24 <ssharm24@ncsu.edu>
2017-12-03 21:34:44 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,24 +6,17 @@ i="0"
 while [ $i -lt 1 ]
 do
 
-cd /home/ubuntu/FlameGraph/
+name=$(date +%F-%T)
+echo $name >> /home/ubuntu/csc_519_devops/dashboard/data/timestamps.txt
+cd /home/ubuntu/FlameGraph
 perf record -F 99 -a -g -- sleep 90
 perf script | ./stackcollapse-perf.pl > out.perf-folded
 ./flamegraph.pl out.perf-folded > perf-kernel.svg
-mv perf-kernel.svg  /home/ubuntu/svg_files/perf-kernel.svg_$(date +%F-%T)
-#mv "perf-kernel.svg" "/home/mpdesai/svg_files$(date -r "perf-kernel" +"%Y%m%d_%H%M%S").jpg"
-
+mv perf-kernel.svg  /home/ubuntu/csc_519_devops/dashboard/data/perf-kernel_$name.svg
 
 perf script | ./stackcollapse-perf.pl > out.perf-folded
 grep -v cpu_idle out.perf-folded | ./flamegraph.pl > nonidle.svg
-#grep ext4 out.perf-folded | ./flamegraph.pl > ext4internals.svg
-#egrep 'system_call.*sys_(read|write)' out.perf-folded | ./flamegraph.pl > rw.svg
-
-mv nonidle.svg  /home/ubuntu/svg_files/nonidle_$(date +%F-%T)
-#mv ext4internals.svg  /home/ubuntu/svg_files/ext4internals_$(date +%F-%T)
-#mv rw.svg  /home/ubuntu/svg_files/rw_$(date +%F-%T)
+mv nonidle.svg  /home/ubuntu/csc_519_devops/dashboard/data/nonidle_$name.svg
 
 sleep 150
 done
-~
-~
